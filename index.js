e75b46acbcea12e113ad8260e999b1e3db2c9410
Dayana <dayana.nashkova@yahoo.com>
2023-12-10 21:48:30 +0000

    feat: make API call with user's input

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
-import OpenAI from 'openai';
-import dotenv from 'dotenv';
-dotenv.config();
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+import openai from './config/open-ai.js';
+import readlineSync from 'readline-sync';
+import colors from 'colors';
 
 async function main() {
-  const chatCompletion = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    messages: [
-      {
-        role: 'user',
-        content: 'What is the capital of Ireland?',
-      },
-    ],
-  });
-  console.log(chatCompletion.data.choices[0]);
+  console.log(colors.bold.green('Welcome to the Chatbot Program!'));
+  console.log(colors.bold.green('You can start chatting with the bot.'));
+
+  while (true) {
+    const userInput = readlineSync.question(colors.yellow('You: '));
+
+    try {
+      // Call the API with user input
+
+      const completion = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: userInput }],
+      });
+
+      // Get completion content
+      const completionText = completion.choices[0].message.content;
+
+      if (userInput.toLowerCase() === 'exit') {
+        console.log(colors.green('Bot: ' + completionText));
+        return;
+      }
+      console.log(colors.green('Bot: ' + completionText));
+    } catch (error) {
+      console.error(colors.red(error));
+    }
+  }
 }
 
 main();
